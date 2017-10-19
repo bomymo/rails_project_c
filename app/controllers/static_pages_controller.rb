09c34089df3a2a2f87f82998afb99220768862eb
bomymo <bomymo@gmail.com>
2017-10-19 12:01:13 -0500

    5.3 Preliminary email form and Sendgrid connection

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,16 @@ class StaticPagesController < ApplicationController
     @featured_product = Product.first
     @products = Product.limit(4)
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+      to: "bomymo@gmail.com",
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   get 'static_pages/landing_page'
+
+  post 'static_pages/thank_you'
   
   root 'static_pages#index'
 #  root 'static_pages#landing_page'
