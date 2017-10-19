140ddf675c1dc8da00372aa93476e9fa5f421b46
bomymo <bomymo@gmail.com>
2017-10-19 15:59:49 -0500

    5.3 Heroku email troubleshooting 3

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,14 @@ class UserMailer < ApplicationMailer
   def contact_form(email, name, message)
     @message = message
     mail(from: email,
-      to: 'your-email@example.com',
+      to: 'bomymo@gmail.com',
     subject: "A new contact form message from #{name}")
   end
 
 end
+
+
+   #ActionMailer::Base.mail(from: @email,
+    #  to: "bomymo@gmail.com",
+    #  subject: "A new contact form message from #{@name}",
+    #  body: @message).deliver_now
