320fb56589ae93012471ba9008b32f267d6296dc
bomymo <bomymo@gmail.com>
2017-10-19 14:48:37 -0500

    5.3 Final production email and formatting

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'bomymo@gmail.com'
   layout 'mailer'
 end
---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,6 @@
+class UserMailerPreview < ActionMailer::Preview
+    def contact_form
+      UserMailer.contact_form("john@example.com", "John", "Hello World!")
+    end
+end
+
