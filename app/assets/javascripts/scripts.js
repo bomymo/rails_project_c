3929c87cda05ce3f1375c61e658afbacf4de7644
bomymo <bomymo@gmail.com>
2017-10-23 10:23:10 -0500

    5.5 Authorization for Orders

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    console.log("document ready");
+    $(".alert").delay(5000).fadeOut(1000);
+  });
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, alert: exception.message
+  end
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    ## To permit attributes while registration i.e. sign up (app/views/devise/registrations/new.html.erb)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    ## To permit attributes while editing a registration (app/views/devise/registrations/edit.html.erb)
+    #devise_parameter_sanitizer.permit(:account_update) << :first_name << :last_name
+  end
 end
