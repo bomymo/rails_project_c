4e0a131453f064a0e17cee6c790d57491b8cf72b
bomymo <bomymo@gmail.com>
2017-10-24 13:23:47 -0500

    5.6 Add Comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
 
   def self.search(search_term)
     if Rails.env.production?        # specific syntax requirement for PostgreSQL
@@ -9,5 +10,12 @@ class Product < ApplicationRecord
     end
   end
 
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
 end
 
